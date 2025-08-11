da71046702abb57413fc734f5dcb4bc3e65f8b98
jlbuild <juliabuildbot@gmail.com>
2025-08-11 15:16:47 +0000

    open62541_jll build 1.4.13+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(OpenSSL_jll)
     JLLWrappers.@init_library_product(
         libopen62541,
-        "lib/libopen62541.1.4.12.dylib",
+        "lib/libopen62541.1.4.13.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
